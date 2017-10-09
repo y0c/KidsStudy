87e3654f87cc9dcad8b400ca660cd01f7109ff7e
LimHoSung <wonder789@naver.com>
2017-10-09 17:55:08 +0900

    TDD 테스트 코드 추가

--- server/test/models/Paper.spec.js
+++ server/test/models/Paper.spec.js
@@ -1,13 +1,71 @@
 import db from "../../models/index";
-import { except } from "chai";
+import { assert } from "chai";
 
-// describe( "Paper Model Test", () => {
+describe( "Paper Model Test", () => {
+    
+    before(() => {
+        return db.sequelize.sync({ force : true })
+            .then(() => {
+                return db.PaperGroup.create({
+                    groupId : "F",
+                    groupTitle : "Test!!!",
+                    createdAt : new Date()
+                }).then( (paperGroup) => {
+                    return db.Paper.create({
+                        groupId : paperGroup.getDataValue("groupId"),
+                        step : "111",
+                        description : "뺄셈을 하세요",
+                        paperTitle : "뺄셈(0)",
+                        createdAt : new Date()
+                    });
+                }). then( paper => {
+                    let paperId = 1;
+                    return db.PaperQuestion.bulkCreate([
+                        { 
+                            paperId,
+                            question : "3333+4444+2222"
+                        },
+                        { 
+                            paperId,
+                            question : "3323+4444+2222"
+                        },
+                        { 
+                            paperId,
+                            question : "3+4444+2222"
+                        },
+                        { 
+                            paperId,
+                            question : "4+4444+2222"
+                        },
+                        { 
+                            paperId,
+                            question : "3333+5+2222"
+                        }
+                    ]);
+                })
+            });
+    });
 
-//     before(() => {
-//         return db.sequelize.sync();
-//     });
 
-//     it( "#Insert and find", ( done ) => {
-//        done(); 
-//     });
-// })
+    it("#Select Paper List with Join PaperQuestion", done => {
+        db.Paper.findOne({
+            where : {
+                paperId : 1
+            }
+        }).then( paper => {
+            assert.isNotNull(paper, "paper is null!");
+            return paper.getPaperQuestions()
+                    .then( list => {
+                        return {
+                            paper,
+                            questions : list 
+                        }
+                    });
+        }).then( obj => {
+            assert.isNotNull(obj.paper);
+            assert.isArray(obj.questions);
+            assert( obj.questions.length > 3);
+            done();
+        })
+    });
+});
---
+++ server/test/routes/studentRouter.spec.js
@@ -0,0 +1,113 @@
+import app from "../../app";
+import { assert } from "chai";
+import request from "supertest";
+
+
+
+describe("StudentRouter Test", () => {
+
+    it("#POST /admin/student", done => {
+        request(app.instance)
+            .post('/admin/student')
+            .send({
+                userId : "test11",
+                userName : "테스트",
+                password : "123123",
+                grade : "초3",
+                role : "student",
+                etc : "비고 테스트"
+            })
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) done(err);
+                
+                assert.equal( res.body.code, "success" );
+                done();
+            });
+    });
+
+    it("#GET /admin/student ", done => {
+        request(app.instance)
+            .get('/admin/student')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end( (err, res) => {
+                if (err) throw err;
+                
+                assert.equal( res.body.code, "success" );
+                assert.isArray( res.body.studentList );
+                done();
+            });
+    });
+
+
+    it("#GET /admin/student/:userId", done => {
+        request(app.instance)
+            .get('/admin/student/test11')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) done(err);
+                
+                assert.equal( res.body.code, "success" );
+                assert.isNotNull( res.body.student );
+                done();
+            });
+    });
+
+    it("#GET /admin/student/paperLog/:userId", done => {
+        request(app.instance)
+            .get('/admin/student/paperLog/test11')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) done(err);
+                
+                assert.equal( res.body.code, "success" );
+                assert.isArray( res.body.userPaperLogList );
+                done();
+            });
+    });
+
+
+    it("#PUT /admin/student/:userId", done => {
+        request(app.instance)
+            .put('/admin/student/test11')
+            .send({
+                userId : "test12",
+                userName : "테스트3333",
+                password : "456456",
+                grade : "초1",
+                role : "student",
+                etc : "비고 테스트"
+            })
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) done(err);
+
+                // assert( res.body.questions.length == 8 );
+                assert.equal( res.body.code, "success" );
+                done();
+            });
+
+    });
+
+    it("#DELETE /admin/student/:userId", done => {
+        request(app.instance)
+            .delete('/admin/student/test12')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) done(err);
+                
+                assert.equal( res.body.code, "success" );
+                done();
+            });
+
+    });
+    
+
+
+});
