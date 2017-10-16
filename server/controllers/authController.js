faa192b160e03a240daf3c33c43483351790e4f7
LimHoSung <wonder789@naver.com>
2017-10-16 22:53:28 +0900

    학생 & 관리자 로그인 동일 권한으로 체크

---
+++ server/controllers/authController.js
@@ -0,0 +1,48 @@
+import db from "../models/index";
+
+const User = db.User;
+
+
+export default {
+    login( req, res, next ){
+        User.findOne({ userId : req.body.userId })
+            .then( user => {
+                if( !user ){
+                    res.send({
+                        code : "success",
+                        message : "존재하지 않는 아이디입니다."
+                    });
+                } else {
+                    
+                    if( user.get("password") != req.body.password ){
+                        res.send({
+                           code : "success",
+                           message : "비밀번호가 일치하지 않습니다." 
+                        });
+                    } else {
+                        req.session.user = user;
+                        res.send({
+                            code : "success",
+                            message : `${user.get("userName")} 님 환영합니다.`,
+                            user
+                        });
+                    }
+                }
+            }) ;       
+    },
+
+    auth( req, res, next ) {
+        let uesrInfo = req.session.user;
+        if( !userInfo ){
+            res.status(401).send({
+                code : "fail",
+                message : "인증되지 않은 사용자입니다."     
+            });
+        } else {
+            res.status(200).send({
+                code : "success",
+                userInfo
+            });
+        }
+    }
+}
